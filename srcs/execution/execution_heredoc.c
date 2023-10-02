4c913f6d44ae46874176afb17f23e3621c5607ea
Flip Van Wijk <fvan-wij@f1r5s13.codam.nl>
2023-10-02 14:56:18 +0200

    heredoc now expands

--- srcs/execution/execution_heredoc.c
+++ srcs/execution/execution_heredoc.c
@@ -33,7 +33,7 @@ char	*expand_heredoc_var(char *heredoc_line, int var_index, char *env_cpy[])
 
 	start = var_index;
 	var_index++;
-	while (heredoc_line[var_index] && heredoc_line[var_index] != ' ' && heredoc_line[var_index] != '$')
+	while (heredoc_line[var_index] && heredoc_line[var_index] != ' ' && heredoc_line[var_index] != '$' && heredoc_line[var_index] != '"' && heredoc_line[var_index] != 39)
 		var_index++;
 	temp = ft_substr(heredoc_line, start, var_index - start);
 	ft_printf("temp: %s.\n", temp);
@@ -47,10 +47,35 @@ char	*expand_heredoc_var(char *heredoc_line, int var_index, char *env_cpy[])
 		ft_printf("str after var: %s\n", string_after_var);
 		temp = ft_getenv(temp + 1, env_cpy);	
 		ft_printf("temp (getenv): %s\n", temp);
-		heredoc_line = ft_strjoin(string_before_var, temp);
-		heredoc_line = ft_strjoin(heredoc_line, string_after_var);
+		heredoc_line = ft_strjoin_and_free(string_before_var, temp);
+		heredoc_line = ft_strjoin_and_free(heredoc_line, string_after_var);
+		free(string_after_var);
+		free(temp);
 		return (heredoc_line);
 	}
+}
+
+char	*expand_heredoc_pid(char *heredoc_line, int pid_token_index)
+{
+	char	*string_before_pid;
+	char	*string_after_pid;
+	int		pid_int;
+	char	*pid_str;
+
+	pid_int = (int)getpid();
+	pid_str = ft_itoa(pid_int);
+	if (pid_token_index > 0)
+	{
+		string_before_pid = ft_substr(heredoc_line, 0, pid_token_index);
+		string_after_pid = ft_substr(heredoc_line, pid_token_index + 2, ft_strlen(heredoc_line));
+		string_after_pid = ft_strjoin_and_free(pid_str, string_after_pid);
+		heredoc_line = ft_strjoin_and_free(string_before_pid, string_after_pid);
+		free(string_after_pid);
+		return (heredoc_line);
+	}
+	else 
+		heredoc_line = ft_strjoin_and_free(pid_str, heredoc_line + pid_token_index + 2);
+	return (heredoc_line);
 }
 
 char	*expand_heredoc_line(char *heredoc_line, char *env_cpy[])
@@ -62,7 +87,13 @@ char	*expand_heredoc_line(char *heredoc_line, char *env_cpy[])
 		return (heredoc_line);
 	while (heredoc_line[i])
 	{
-		if (heredoc_line[i] == '$' && heredoc_line[i + 1] != '\0' && ft_isalpha(heredoc_line[i + 1]) == 1)
+		if (heredoc_line[i] == '$' && heredoc_line[i + 1] != '\0' && heredoc_line[i + 1] == '$')
+		{
+			heredoc_line = expand_heredoc_pid(heredoc_line, i);
+			if (heredoc_line[i + 1])
+				i++;
+		}
+		else if (heredoc_line[i] == '$' && heredoc_line[i + 1] != '\0' && ft_isalpha(heredoc_line[i + 1]) == 1)
 			heredoc_line = expand_heredoc_var(heredoc_line, i, env_cpy);
 		i++;
 	}
