6bac22d5865290cdf6c8e42b8904608be055a79c
Flip Van Wijk <fvan-wij@f1r6s10.codam.nl>
2023-10-10 13:14:18 +0200

    Fixed -nnn in echo

--- srcs/builtins/builtin_echo.c
+++ srcs/builtins/builtin_echo.c
@@ -4,20 +4,6 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-// static bool	print_env(char *input, char *envp[], int *i)
-// {
-// 	char	*value;
-//
-// 	value = NULL;
-// 	if (arg_is_env(&input[*i], &value, envp) == 1)
-// 	{
-// 		*i += ft_printf("%s", value) + 1;
-// 		return (free(value), true);
-// 	}
-// 	return (free(value), false);
-// }
-//
-
 static int	output_trailing_newline(char *raw_input)
 {
 	int	i;
@@ -27,22 +13,23 @@ static int	output_trailing_newline(char *raw_input)
 	j = 0;
 	while (raw_input[i])
 	{
-		if (raw_input[i] == '-' && raw_input[i + 1] != '\0'
-			&& raw_input[i + 1] == 'n' && ft_iswhitespace(raw_input[i + 2]))
+		if (raw_input[i] == '-')
 		{
-			j = i + 2;
-			while (raw_input[j])
-			{
-				if (ft_iswhitespace(raw_input[j]))
-					j++;
-				if (!ft_iswhitespace(raw_input[j]) && ft_isprint(raw_input[j]))
-					return (j);
+			j = i + 1;
+			while (raw_input[j] && !ft_iswhitespace (raw_input[j])
+				&& raw_input[j] == 'n' && raw_input[j] != '-')
 				j++;
-			}
+			if (ft_isalpha(raw_input[j]))
+				return (i);
+			i = j;
 		}
+		else if (ft_isascii(raw_input[i]))
+			return (i);
 		i++;
 	}
-	return (0);
+	if (i > (int)ft_strlen(raw_input))
+		return (-1);
+	return (j + 1);
 }
 
 static bool	print_pid(char *input, int *i)
@@ -91,12 +78,15 @@ static void	expand_and_print_input(char *input, char *envp[])
 	i = 0;
 	while (input[i])
 	{
-		// if (print_env(input, envp, &i))
-		// 	continue ;
 		if (print_pid(input, &i))
 			continue ;
 		else if (print_home(input, envp, &i))
 			continue ;
+		else if (ft_strncmp(&input[i], "$?", 2) == 0)
+		{
+			ft_printf("%d", g_exit_status);
+			i++;
+		}
 		else if (input[i] != '\0')
 			write(STDIN_FILENO, &input[i], 1);
 		i++;
@@ -117,11 +107,10 @@ void	execute_echo(char **raw_input, char *envp[])
 		&& raw_input[1][2] == ' ' && raw_input[1][3] == '\0')
 		return ;
 	line_start = output_trailing_newline(raw_input[1]);
+	if (line_start == -1)
+		return ;
 	line = &raw_input[1][line_start];
-	if (ft_strncmp(line, "$?", ft_strlen(line)) == 0)
-		ft_printf("%d", g_exit_status);
-	else
-		expand_and_print_input(line, envp);
+	expand_and_print_input(line, envp);
 	if (line_start == 0)
 		ft_printf("\n");
 	g_exit_status = 0;
