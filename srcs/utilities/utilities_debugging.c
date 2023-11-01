4963171e238e71c2aba81056d56f860db3ab7a52
Flip Van Wijk <fvan-wij@f1r5s13.codam.nl>
2023-11-01 18:08:36 +0100

    Fixed merge conflict

--- srcs/utilities/utilities_debugging.c
+++ srcs/utilities/utilities_debugging.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   utilities_debugging.c                             :+:    :+:             */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dhussain <dhussain@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 15:09:40 by dhussain          #+#    #+#             */
-/*   Updated: 2023/11/01 16:36:24 by fvan-wij      ########   odam.nl         */
-/*                                                                            */
-/* ************************************************************************** */
-
 // Global array - used to print out the enum strings.
 // Must be deleted before handing in the project.
 #include "../../includes/minishell.h"
