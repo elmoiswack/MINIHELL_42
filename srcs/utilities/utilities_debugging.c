fe8415fbe8ebefa5305e0b2610b9c06b3cde152c
Dante Hussain <dhussain@f1r5s20.codam.nl>
2023-11-17 12:35:32 +0100

    nani da fuq

--- srcs/utilities/utilities_debugging.c
+++ srcs/utilities/utilities_debugging.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   utilities_debugging.c                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dhussain <dhussain@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/26 15:09:40 by dhussain          #+#    #+#             */
-/*   Updated: 2023/11/17 11:10:39 by dhussain         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 // Global array - used to print out the enum strings.
 // Must be deleted before handing in the project.
 #include "../../includes/minishell.h"
